9ae037f1c33155846a8e19ca6a2b14da97da9a47
KelmurzinDanil <143899591+KelmurzinDanil@users.noreply.github.com>
2023-12-14 17:40:06 +0300

    Add files via upload

---
+++ 14.12.2023/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Reflection;  // Подключаем рефлексию
+
+namespace _14._12._2023
+{
+    internal class Program
+    {
+        public static void OutputOfNumbers() // Вывод чисел от 1 до 10
+        {
+            for (int i = 0; i <= 10; i++)
+            {
+                Thread.Sleep(300);
+                Console.Write($"{i} ") ;
+            }
+        }
+        public static int CalculatingTheFactorial(int k) // Вычисление факториала
+        {
+            if (k < 0)
+            {
+                Console.WriteLine("Факториал отрицательного числа не высчитывается");
+            }
+            if (k == 1 || k == 0)
+            {
+                return 1;
+            }
+
+            return k * CalculatingTheFactorial(k - 1);
+        }
+        public static async Task<int> AsyncCalculatingTheFactorial(int k) // Вычисление факториала асинхронно
+        {   
+            await Task.Delay(8000); // Задержка асинхронного метода
+            return await Task.Run(() => CalculatingTheFactorial(k));
+        }
+        public static double CalculatingSquare(int number)
+        {
+            return number * number;
+        }
+         async static Task Main(string[] args)
+        {
+            Console.WriteLine("Упр. 1");
+            Thread thread1 = new Thread(new ThreadStart(OutputOfNumbers));
+            Thread thread2 = new Thread(new ThreadStart(OutputOfNumbers));
+            Thread thread3 = new Thread(new ThreadStart(OutputOfNumbers));
+            thread1.Start();
+            thread2.Start();
+            thread3.Start();
+
+            try
+            {
+                Thread.Sleep(3500);
+                Console.WriteLine("\nУпр. 2");
+                Console.WriteLine("Введите целое число: ");
+                int n = Int32.Parse(Console.ReadLine());
+                Console.WriteLine("Квадрат: " + CalculatingSquare(n));
+                Console.WriteLine("Ждем 8 секунд");
+                Console.WriteLine("Факториал: " + await AsyncCalculatingTheFactorial(n));
+            }
+            catch 
+            {
+                Console.WriteLine("Вы ввели что-то не то");
+            }
+            Console.WriteLine("Упр. 3");
+            Type typeRef1 = typeof(Ref1);
+            foreach(MemberInfo memberInfo in typeRef1.GetMethods())
+            {
+                Console.WriteLine($"{memberInfo.Name}");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ 14.12.2023/Ref1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace _14._12._2023
+{
+    public class Ref1
+    {
+        public void Main(string[] args) // Убрал static, потому что выдавало ошибку
+        {
+            Console.WriteLine(Output());
+            Console.WriteLine(AddInts(1, 2));
+        }
+        public string Output()
+        {
+            return "Test-Output";
+        }
+        public int AddInts(int i1, int i2)
+        {
+            return i1 + i2;
+        }
+    }
+}
